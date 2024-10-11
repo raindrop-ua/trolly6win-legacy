8000fad0f709a74764467263268d271b4b94bdf8
Anton Sizov <raindropua@gmail.com>
2024-10-11 15:30:40 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,22 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Trolly6</h1>
-      <div className="card">
-          Loading...
-      </div>
+      <h1>TrollySix</h1>
+        <div className="card">
+            <div>
+                <div>
+                    <button type="button">Weekday</button>
+                    <button type="button">Weekend</button>
+                </div>
+            </div>
+            <div>
+                <h3>Start point</h3>
+                <div>
+                    <button type="button">Pridniprovsk City</button>
+                    <button type="button">Cathedral Square</button>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
