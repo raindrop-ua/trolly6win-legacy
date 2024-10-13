3f545d724ac25f7899dd17c4c63138abd6aebe0b
Anton Sizov <raindropua@gmail.com>
2024-10-13 20:27:25 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,25 @@ function App() {
         <>
             <h1>TrollySix</h1>
             <p>Timetable for trolleybus route number 6 in the city of Dnipro.</p>
-            <div className="card">
-                <div>
-                    <div>
-                        <ButtonsGroup>
-                            <Button onClick={() => console.log('Pressed')}>Weekday</Button>
-                            <Button onClick={() => console.log('Pressed')}>Weekend</Button>
-                        </ButtonsGroup>
-                    </div>
-                </div>
-                <div>
-                    <h3>Start point</h3>
-                    <div>
-                        <ButtonsGroup>
-                            <Button onClick={() => console.log('Pressed')}>Pridniprovsk City</Button>
-                            <Button onClick={() => console.log('Pressed')}>Cathedral Square</Button>
-                        </ButtonsGroup>
-                    </div>
-                </div>
-            </div>
+            {/*<div className="card">*/}
+            {/*    <div>*/}
+            {/*        <div>*/}
+            {/*            <ButtonsGroup>*/}
+            {/*                <Button onClick={() => console.log('Pressed')}>Weekday</Button>*/}
+            {/*                <Button onClick={() => console.log('Pressed')}>Weekend</Button>*/}
+            {/*            </ButtonsGroup>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <h3>Start point</h3>*/}
+            {/*        <div>*/}
+            {/*            <ButtonsGroup>*/}
+            {/*                <Button onClick={() => console.log('Pressed')}>Pridniprovsk City</Button>*/}
+            {/*                <Button onClick={() => console.log('Pressed')}>Cathedral Square</Button>*/}
+            {/*            </ButtonsGroup>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </>
     )
 }
