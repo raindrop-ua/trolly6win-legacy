d646a9cb412dc06d1b4d4b3cd304176cf4415492
Anton Sizov <raindropua@gmail.com>
2024-10-13 16:13:35 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <h1>TrollySix</h1>
-        <div className="card">
-            <div>
+    return (
+        <>
+            <h1>TrollySix</h1>
+            <p>Timetable for trolleybus route number 6 in the city of Dnipro.</p>
+            <div className="card">
                 <div>
-                    <button type="button">Weekday</button>
-                    <button type="button">Weekend</button>
+                    <div>
+                        <button type="button">Weekday</button>
+                        <button type="button">Weekend</button>
+                    </div>
+                </div>
+                <div>
+                    <h3>Start point</h3>
+                    <div>
+                        <button type="button">Pridniprovsk City</button>
+                        <button type="button">Cathedral Square</button>
+                    </div>
                 </div>
             </div>
-            <div>
-                <h3>Start point</h3>
-                <div>
-                    <button type="button">Pridniprovsk City</button>
-                    <button type="button">Cathedral Square</button>
-                </div>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
