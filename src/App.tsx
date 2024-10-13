764920d1a74e4d7801e9ff44ea8274a02b01c703
Anton Sizov <raindropua@gmail.com>
2024-10-13 20:28:46 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import './App.scss'
-import Button from "./components/Button";
-import ButtonsGroup from "./components/ButtonsGroup";
 
 function App() {
     return (
         <>
             <h1>TrollySix</h1>
             <p>Timetable for trolleybus route number 6 in the city of Dnipro.</p>
-            {/*<div className="card">*/}
-            {/*    <div>*/}
-            {/*        <div>*/}
-            {/*            <ButtonsGroup>*/}
-            {/*                <Button onClick={() => console.log('Pressed')}>Weekday</Button>*/}
-            {/*                <Button onClick={() => console.log('Pressed')}>Weekend</Button>*/}
-            {/*            </ButtonsGroup>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*    <div>*/}
-            {/*        <h3>Start point</h3>*/}
-            {/*        <div>*/}
-            {/*            <ButtonsGroup>*/}
-            {/*                <Button onClick={() => console.log('Pressed')}>Pridniprovsk City</Button>*/}
-            {/*                <Button onClick={() => console.log('Pressed')}>Cathedral Square</Button>*/}
-            {/*            </ButtonsGroup>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </>
     )
 }
