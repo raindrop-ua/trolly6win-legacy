265ee8a4ebcbd8b5b9ccfff45071eddb576a3ffc
Anton Sizov <raindropua@gmail.com>
2024-11-26 13:51:42 +0200

    Update Footer version and styles
    Increment the app version to 1.12 and modify the footer structure. Added a new CSS class for the date styling in the footer component.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
 	return (
 		<footer className={styles.FooterSection}>
 			<div className={styles.Footer}>
-				<div>V1.11 &copy; 2024.</div>
+				<div>V1.12 &copy; 2024.</div>
 				<div>Coded with love for the Web and trolleybuses.</div>
 			</div>
-			<div className={styles.Footer}>
+			<div className={styles.FooterDate}>
 				<div>{new Date().toString()}</div>
 			</div>
 		</footer>
