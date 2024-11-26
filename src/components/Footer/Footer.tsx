bc34f8ddd14fbfedbeacf5ffdbf792afd2ace2ed
Anton Sizov <raindropua@gmail.com>
2024-11-26 08:24:52 +0200

    Update version to 1.11
    Bump the application version from 1.10 to 1.11 in both Footer component and package.json. This reflects recent changes and improvements in the project.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<footer className={styles.FooterSection}>
 			<div className={styles.Footer}>
-				<div>V1.10 &copy; 2024.</div>
+				<div>V1.11 &copy; 2024.</div>
 				<div>Coded with love for the Web and trolleybuses.</div>
 			</div>
 		</footer>
