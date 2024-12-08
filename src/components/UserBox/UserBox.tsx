d78ff07a4ceaa8cebbcd55aad149cf13f899e315
Anton Sizov <raindropua@gmail.com>
2024-12-08 18:19:41 +0200

    Enhance UserBox menu with outside click handling
    Refactored UserBox component to include useRef and useEffect hooks for managing menu visibility when clicking outside. Updated styles for improved menu layout and spacing adjustments in AvatarMenu and TimeListFilter components. Added clear code comments for better maintainability.

--- src/components/UserBox/UserBox.tsx
+++ src/components/UserBox/UserBox.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import styles from './UserBox.module.scss'
@@ -11,6 +11,8 @@ import useToastStore from '@/store/toastStore'
 const UserBox = () => {
 	const pathname = usePathname()
 	const [showMenu, setShowMenu] = React.useState(false)
+	const menuRef = useRef<HTMLDivElement>(null) // Реф для меню
+	const buttonRef = useRef<HTMLButtonElement>(null) // Реф для кнопки Avatar
 	const { isLoggedIn } = useAuthStore()
 	const { email } = useUserStore()
 	const { addToast } = useToastStore()
@@ -27,6 +29,28 @@ const UserBox = () => {
 		setShowMenu((prevState) => !prevState)
 	}
 
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				menuRef.current &&
+				!menuRef.current.contains(event.target as Node) &&
+				buttonRef.current &&
+				!buttonRef.current.contains(event.target as Node)
+			) {
+				setShowMenu(false)
+			}
+		}
+
+		document.addEventListener('click', handleClickOutside)
+		return () => {
+			document.removeEventListener('click', handleClickOutside)
+		}
+	}, [])
+
+	const handleMenuClick = () => {
+		setShowMenu(false)
+	}
+
 	if (pathname === '/login') {
 		return null
 	}
@@ -39,11 +63,21 @@ const UserBox = () => {
 				</Link>
 			) : (
 				<div className={styles.AvatarBlock}>
-					<button className={styles.Avatar} onClick={handleAvatar}>
+					<button
+						ref={buttonRef}
+						className={styles.Avatar}
+						onClick={handleAvatar}
+					>
 						{email && email[0].toUpperCase()}
 					</button>
 					{showMenu && (
-						<div className={styles.AvatarMenu}>
+						<div
+							ref={menuRef}
+							className={styles.AvatarMenu}
+							onClick={handleMenuClick}
+						>
+							<Link href={'/profile'}>Profile</Link>
+							<Link href={'/editor'}>Editor</Link>
 							<button onClick={showToast}>
 								<span>Log out</span>
 							</button>
